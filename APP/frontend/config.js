26f988e2730f9770eeec0167b4a90c35c98f09c2
Lucas Luan <118564181+Lucas8181luan@users.noreply.github.com>
2026-08-09 01:05:31 -0300

    Update API_URL comment in config.js

--- APP/frontend/config.js
+++ APP/frontend/config.js
@@ -1,3 +1,3 @@
 // Coloque aqui a URL do seu backend no Render depois do deploy.
-// Ex: "https://ifp-backend.onrender.com"
+// Ex: "https://comissionamento-backend.onrender.com"
 const API_URL = "https://internal-reports.onrender.com";
